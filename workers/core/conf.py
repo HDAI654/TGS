eb7607bfe5b159d216f2383a69ea8c3b547c4463
Hamed <kolbop.code@gmail.com>
2026-07-15 13:20:53 +0330

    fix some bugs in dockerfiles

--- workers/core/conf.py
+++ workers/core/conf.py
@@ -1,1 +1,24 @@
-from src.modules.core.conf import Config
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+env_file = BASE_DIR / ".env"
+
+if os.getenv("APP_NAME", None) is None:
+    load_dotenv(env_file)
+
+
+class Config:
+    # App
+    APP_NAME: str = os.getenv("APP_NAME", "MyApp")
+    APP_ENV: str = os.getenv("APP_ENV", "development")
+
+    # DB
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL",
+        "sqlite+aiosqlite:///:memory:",
+    )
+
+    # Cache
+    REDIS_URL: str = os.getenv("REDIS_URL", "redis://redis:6379/0")
